480e2605419f318bb33060158d4fc24fb476744b
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-04-14 22:55:27 +0600

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import "@/styles/globals.css";
-import type { Metadata } from "next";
-import Script from "next/script";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bangladesh Constitution Assistant",
   description: "Get answers about the Constitution of Bangladesh",
   generator: "By Ikbal Nayem",
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -20,12 +21,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
-        <Script type="module" src="https://md-block.verou.me/md-block.js" strategy="lazyOnload"/>
+        <Script type="module" src="https://md-block.verou.me/md-block.js" strategy="lazyOnload" />
       </body>
+      <GoogleAnalytics gaId={process.env.GA_TRACKING_ID || ""} />
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
